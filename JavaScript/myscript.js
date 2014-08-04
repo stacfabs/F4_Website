621127ff259d9d2beeaf7069a349174931ca300a
Stacy Fabian <stacy.fabian@gmail.com>
2014-08-04 00:30:57 -0700

    Initial Commit

---
+++ JavaScript/myscript.js
@@ -0,0 +1,22 @@
+// Do a couple of console.logs
+
+console.log("Hello from myscript.js");
+console.log("Hello again"); // This is not needed
+
+/*
+var name = prompt("What is your name?");
+alert("Welcome " + name + "!");
+
+name = "Nick";
+console.log("The user's name is " + name);
+*/
+
+console.log("Before");
+
+if (9) {
+	console.log("If block");
+} else {
+	console.log("Else block");
+}
+
+console.log("After");
